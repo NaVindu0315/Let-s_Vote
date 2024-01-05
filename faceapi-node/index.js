373adb112063df47c1199eb40f172866205aefc6
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-01-05 19:30:27 +0530

    still not working

--- faceapi-node/index.js
+++ faceapi-node/index.js
@@ -1,54 +1,26 @@
 const axios = require('axios');
-const subscriptionKey = 'b87c9d9fa11b44f0a8df68b454d494f4';
-const uriBase = 'https://nav.cognitiveservices.azure.com/face/v1.0/detect';
+
+// Replace 'YOUR_API_KEY' with your actual API key
+const apiKey = 'b87c9d9fa11b44f0a8df68b454d494f4';
+
+// Define the endpoint URL for face detection
+const endpoint = 'https://nav.cognitiveservices.azure.com/face/v1.0/detect';
 
 
-const analyzeImages = async (imageUrl1, imageUrl2) => {
- const detectImage1 = async () => {
-    try {
-      const response = await axios.post(
-        uriBase,
-        { url: imageUrl1 },
-        { headers: { 'Ocp-Apim-Subscription-Key': subscriptionKey } }
-      );
-      return response.data[0].faceId;
-    } catch (error) {
-      console.error('Error in detectImage1:', error);
-    }
- };
-
- const detectImage2 = async () => {
-    try {
-      const response = await axios.post(
-        uriBase,
-        { url: imageUrl2 },
-        { headers: { 'Ocp-Apim-Subscription-Key': subscriptionKey } }
-      );
-      return response.data[0].faceId;
-    } catch (error) {
-      console.error('Error in detectImage2:', error);
-    }
- };
-
- const faceId1 = await detectImage1();
- const faceId2 = await detectImage2();
-
- const verifyFaces = async () => {
-    try {
-      const response = await axios.post(
-        `${uriBase}/verify`,
-        { faceId1, faceId2 },
-        { headers: { 'Ocp-Apim-Subscription-Key': subscriptionKey } }
-      );
-      console.log('Faces verification result:', response.data);
-    } catch (error) {
-      console.error('Error in verifyFaces:', error);
-    }
- };
-
- await verifyFaces();
+// Set up the request headers
+const headers = {
+ 'Content-Type': 'application/json',
+ 'Ocp-Apim-Subscription-Key': apiKey
 };
 
-const imageUrl1 = 'https://firebasestorage.googleapis.com/v0/b/cam-test-2-78bd3.appspot.com/o/images%2F2023-12-01%2012%3A39%3A43.963704.jpg?alt=media&token=f9258809-b452-4c64-a4ce-d88718e6f55e';
-const imageUrl2 = 'https://firebasestorage.googleapis.com/v0/b/cam-test-2-78bd3.appspot.com/o/images%2F2023-12-01%2012%3A40%3A23.675609.jpg?alt=media&token=75db23b3-db0f-4efd-8bce-0ebcc32271f2';
-analyzeImages(imageUrl1, imageUrl2);
+// Define the request body (optional)
+const body = {
+ returnFaceId: true,
+ returnFaceLandmarks: false,
+ returnFaceAttributes: ['age', 'gender', 'headPose', 'smile', 'facialHair', 'glasses', 'emotion', 'hair', 'makeup', 'occlusion', 'accessories', 'blur', 'exposure', 'noise']
+};
+
+// Make the request to the Azure Face API
+axios.post(endpoint, body, { headers: headers })
+ .then(response => console.log(response.data))
+ .catch(error => console.error(error));
