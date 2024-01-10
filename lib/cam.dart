876d372da1dda71212dd19782a31d299ce465634
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-01-10 12:46:28 +0530

    added http parser

--- lib/cam.dart
+++ lib/cam.dart
@@ -80,7 +80,7 @@ class _MyHomePageState extends State<MyHomePage> {
   ///my face api function
   void callCompareFacesAPI() async {
     try {
-      final url = Uri.parse('http://localhost:3000/api/compareFaces');
+      final url = Uri.parse('http://localhost:3000/compareFaces');
       final requestBody = jsonEncode({
         "faceurl1":
             "https://firebasestorage.googleapis.com/v0/b/navindu-store.appspot.com/o/face-api%20images%2Fnngi_2.jpeg?alt=media&token=af022352-c7f2-4b21-9f4a-036bc857e6b0",
@@ -102,6 +102,9 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   ///end
+  ///faceapi try
+
+  ///face api end
 
   /// frebase uploading function end
   @override
