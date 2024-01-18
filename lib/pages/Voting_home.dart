24720db8db9a91b6d02e8fc7343bcbc17579b11d
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-01-18 18:33:53 +0530

    customizing voting home page

--- lib/pages/Voting_home.dart
+++ lib/pages/Voting_home.dart
@@ -1,4 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:lets_vote/cam.dart';
+import 'package:lets_vote/pages/comparing_page.dart';
+import 'package:lets_vote/pages/signup.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+late User loggedinuser;
+late String client;
 
 class voting_home extends StatefulWidget {
   const voting_home({Key? key}) : super(key: key);
