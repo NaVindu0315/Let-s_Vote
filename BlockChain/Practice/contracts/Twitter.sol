665263c90aea311b2b31dfea0aa7de497675933c
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-01-31 20:48:16 +0530

    added TweetStruct

--- BlockChain/Practice/contracts/Twitter.sol
+++ BlockChain/Practice/contracts/Twitter.sol
@@ -4,25 +4,48 @@
 
 contract Twitter {
 
+    ///struct for the tweets
+    struct Tweet{
+        address author;
+        string content;
+        uint256 timestamp;
+        uint256 likes;
+    }
+
+    ///end struct
+
 
     //CODE
-    mapping (address => string[] ) public tweets;
+    mapping (address => Tweet[] ) public tweets;
+
+
     
         function createTweet(string memory _tweet) public {
-            tweets[msg.sender].push(_tweet);
+
+
+            Tweet memory newTweet = Tweet({
+                author : msg.sender,
+                content : _tweet,
+                timestamp : block.timestamp,
+                likes : 0
+
+            });
+
+
+            tweets[msg.sender].push(newTweet);
         }
         //to store tweets in array 
         //.push(_tweeet) added
         //uint i added in get tweet function
 
     //to get the specific tweet
-        function getTweet(address _owner,uint _i) public view returns (string memory){
+        function getTweet(address _owner,uint _i) public view returns (Tweet memory){
             return tweets[_owner][_i];
 
         }
         
         //to get all the tweets
-        function getalltweets(address _owner) public view returns (string[] memory)
+        function getalltweets(address _owner) public view returns (Tweet[] memory)
         {
             return tweets[_owner];
         }    
@@ -30,4 +53,3 @@ contract Twitter {
 
 
 }
-
