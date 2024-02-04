ecd3ad7cc323a120c72da887330e3b9cc3dac07c
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-02-04 18:34:11 +0530

    added constructor

--- BlockChain/Practice/contracts/modifier.sol
+++ BlockChain/Practice/contracts/modifier.sol
@@ -7,5 +7,12 @@ contract PausableToken
     address public owner;
     bool public paused;
     mapping (address => uint) public balances;
+
+    constructor()
+    {
+        owner =msg.sender;
+        paused = false;
+        balances[owner] = 1000;
+    }
     
 }
