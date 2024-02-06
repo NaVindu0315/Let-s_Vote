5d75e1df41c1552f002005d158b360245d7ab83f
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-02-06 22:18:23 +0530

    chngged button textcolor

--- lib/Colors/colors.dart
+++ lib/Colors/colors.dart
@@ -7,7 +7,8 @@ class AppColors {
 
   static const Color backgroundcolor = Color(0xFF0A1930); // for the background
   static const Color buttoncolor = Color(0xFF66FFE1); //for buttons
-  static const Color accent = Color(0xFF33AAFF); // Your actual accent color
+  static const Color buttontextcolor =
+      Color(0xFF033E44); // Your actual accent color
   static const Color background = Colors.white; // Or your preferred background
   static const Color text = Colors.black; // Or your preferred text color
 }
