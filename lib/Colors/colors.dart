22c35c557bff4a582a18028c7d4f2c126746a355
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-02-06 22:05:37 +0530

    colors

--- lib/Colors/colors.dart
+++ lib/Colors/colors.dart
@@ -1,1 +1,14 @@
+import 'dart:ui';
 
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const Color primary = Color(0xFF007991); // Your actual primary color
+  static const Color secondary =
+      Color(0xFFF9AA33); // Your actual secondary color
+  static const Color accent = Color(0xFF33AAFF); // Your actual accent color
+  static const Color background = Colors.white; // Or your preferred background
+  static const Color text = Colors.black; // Or your preferred text color
+}
