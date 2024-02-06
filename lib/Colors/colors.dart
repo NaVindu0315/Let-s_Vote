33b691bf084a3a5a9e1c7ac25598497ed7a8a516
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-02-06 22:11:42 +0530

    added button color

--- lib/Colors/colors.dart
+++ lib/Colors/colors.dart
@@ -5,10 +5,8 @@ import 'package:flutter/material.dart';
 class AppColors {
   AppColors._();
 
-  static const Color backgroundcolor =
-      Color(0xFF0A1930); // Your actual primary color
-  static const Color secondary =
-      Color(0xFFF9AA33); // Your actual secondary color
+  static const Color backgroundcolor = Color(0xFF0A1930); // for the background
+  static const Color buttoncolor = Color(0xFF66FFE1); //for buttons
   static const Color accent = Color(0xFF33AAFF); // Your actual accent color
   static const Color background = Colors.white; // Or your preferred background
   static const Color text = Colors.black; // Or your preferred text color
