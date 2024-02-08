484cb12d0daf5a77f3bc169ce3842264c1b7ca53
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-02-08 21:51:44 +0530

    chgned adrs

--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -345,11 +345,34 @@ class _signupState extends State<signup> {
                         decoration: InputDecoration(
                           prefixIcon: Icon(
                             Icons.location_city,
+                            color:
+                                Colors.white, // Set prefix icon color to white
                           ),
                           labelText: 'Address',
-                          border: OutlineInputBorder(),
+                          labelStyle: TextStyle(
+                              color: Colors
+                                  .white), // Set label text color to white
+                          hintText: 'Enter your address',
+                          hintStyle: TextStyle(
+                              color: Colors.white.withOpacity(
+                                  0.6)), // Set hint text color with opacity
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide(
+                                color:
+                                    Colors.white), // Set border color to white
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                                color: Colors.blueAccent,
+                                width:
+                                    2.0), // Set focused border color and width
+                          ),
                         ),
+                        style: const TextStyle(
+                            color:
+                                Colors.white), // Set typed text color to white
                       ),
+
                       //birthday
                       TextFormField(
                         onChanged: (value) {
