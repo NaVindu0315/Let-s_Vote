bc225fa4607c7c7399c5e43c2793f292b07a478d
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-02-09 21:36:59 +0530

    created a sample drawer

---
+++ lib/functions/drawer_sample.dart
@@ -0,0 +1,205 @@
+import 'package:flutter/material.dart';
+import 'package:lets_vote/Colors/colors.dart';
+import 'package:lets_vote/cam.dart';
+import 'package:lets_vote/pages/comparing_page.dart';
+import 'package:lets_vote/pages/signup.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import '../pages/Voting_home.dart';
+
+late User loggedinuser;
+late String client;
+
+class sample_drawer extends StatefulWidget {
+  const sample_drawer({Key? key}) : super(key: key);
+
+  @override
+  State<sample_drawer> createState() => _sample_drawerState();
+}
+
+class _sample_drawerState extends State<sample_drawer> {
+  final _firestore = FirebaseFirestore.instance;
+  final _auth = FirebaseAuth.instance;
+  TextEditingController loggedinusercontroller = TextEditingController();
+  TextEditingController imageurlcontroller = TextEditingController();
+
+  ///to get the current user
+  @override
+  void initState() {
+    super.initState();
+    getcurrentuser();
+  }
+
+  void getcurrentuser() async {
+    try {
+      // final user = await _auth.currentUser();
+      ///yata line eka chatgpt code ekk meka gatte uda line eke error ekk ena hinda hrytama scene eka terenne na
+      User? user = FirebaseAuth.instance.currentUser;
+      if (user != null) {
+        loggedinuser = user;
+        client = loggedinuser.email!;
+
+        ///i have to call the getdatafrm the function here and parse client as a parameter
+
+        print(loggedinuser.email);
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: SafeArea(
+      child: StreamBuilder<DocumentSnapshot>(
+          stream: FirebaseFirestore.instance
+              .collection('users')
+              .doc(client)
+              .snapshots(),
+          builder:
+              (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+            if (snapshot.hasData) {
+              var data = snapshot.data;
+              return Scaffold(
+                resizeToAvoidBottomInset: false,
+
+                ///navigation bar eka iwrii
+                ///drawer
+                drawer: Drawer(
+                  width: 300,
+                  child: Container(
+                    color: AppColors.backgroundcolor, //color of list tiles
+                    // Add a ListView to ensures the user can scroll
+                    child: ListView(
+                      // Remove if there are any padding from the ListView.
+                      padding: EdgeInsets.zero,
+                      children: <Widget>[
+                        UserAccountsDrawerHeader(
+                          decoration: BoxDecoration(
+                            color: Color(0xFFFEB3DE), //color of drawer header
+                          ),
+                          accountName: Text(
+                            '${data!['username']}',
+                            style: TextStyle(
+                                color: AppColors.backgroundcolor,
+                                fontSize: 22,
+                                fontWeight: FontWeight.bold,
+                                fontFamily: "Roboto"),
+                          ),
+                          accountEmail: Text(
+                            client,
+                            style: TextStyle(
+                                color: AppColors.backgroundcolor,
+                                fontSize: 17,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          currentAccountPicture: CircleAvatar(
+                            radius: 50,
+                            backgroundImage: NetworkImage('${data!['url']}'),
+                          ),
+                        ),
+
+                        //Home
+                        Builder(builder: (context) {
+                          return ListTile(
+                            leading: Icon(
+                              Icons.home,
+                              color: Colors.white,
+                            ),
+                            title: const Text('Home',
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 17)),
+                            onTap: () {
+                              /*   Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => DashBoard()),
+                              );*/
+                            },
+                          );
+                        }),
+                        //Cam page
+                        Builder(builder: (context) {
+                          return ListTile(
+                            leading: Icon(
+                              Icons.camera_alt,
+                              color: Colors.white,
+                            ),
+                            title: const Text('Image Testing Page',
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 17)),
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => myapp()),
+                              );
+                            },
+                          );
+                        }),
+                        //Announcement
+                        Builder(builder: (context) {
+                          return ListTile(
+                            leading: Icon(
+                              Icons.face,
+                              color: Colors.white,
+                            ),
+                            title: const Text('Face Comparing Test',
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 17)),
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => Compare_page()),
+                              );
+                            },
+                          );
+                        }),
+
+                        ///voting home
+                        Builder(builder: (context) {
+                          return ListTile(
+                            leading: Icon(
+                              Icons.how_to_vote,
+                              color: Colors.white,
+                            ),
+                            title: const Text('Voting Home',
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 17)),
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => voting_home()),
+                              );
+                            },
+                          );
+                        }),
+                      ],
+                    ),
+                  ),
+                ),
+
+                ///drawwe end
+                appBar: AppBar(
+                  backgroundColor: Color(0xFFA888EB),
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(30),
+                      bottomRight: Radius.circular(30),
+                    ),
+                  ),
+                  title: Text('Welcome To Lets Vote'),
+
+                  //centerTitle: true,
+                ),
+              );
+            }
+            return CircularProgressIndicator();
+          }),
+    ));
+  }
+}
