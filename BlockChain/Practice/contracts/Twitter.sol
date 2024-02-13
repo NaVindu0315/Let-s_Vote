78d0c2dda4b65ffde09a42beac0a1e2216ecfa71
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-02-13 20:11:41 +0530

    added  like and unlike functions

--- BlockChain/Practice/contracts/Twitter.sol
+++ BlockChain/Practice/contracts/Twitter.sol
@@ -4,10 +4,11 @@ pragma solidity ^0.8.22;
 
 contract Twitter {
 
-    uint16 constant MAX_TWEET_Length = 280;
+    uint16 public  MAX_TWEET_Length = 280;
 
     ///struct for the tweets
     struct Tweet{
+        uint256 id;
         address author;
         string content;
         uint256 timestamp;
@@ -20,6 +21,25 @@ contract Twitter {
     //CODE
     mapping (address => Tweet[] ) public tweets;
 
+    address public owner;
+
+    ///creating the constructor
+    constructor()
+    {
+        owner  = msg.sender;
+    }
+    ///modifier
+    modifier onlyOwner()
+    {
+        require(msg.sender ==owner,"YOU ARE NOT THE OWNER"); 
+        _;   
+    }
+
+    //creating a function to chnge tweet lenght
+    function changeTweetLength(uint16 newTweetLength) public onlyOwner{
+        MAX_TWEET_Length = newTweetLength;
+    }
+
 
     
         function createTweet(string memory _tweet) public {
@@ -28,6 +48,7 @@ contract Twitter {
                 require(bytes(_tweet).length<=MAX_TWEET_Length,"Tweet is too Long");
 
             Tweet memory newTweet = Tweet({
+                id:tweets[msg.sender].length,
                 author : msg.sender,
                 content : _tweet,
                 timestamp : block.timestamp,
@@ -41,6 +62,21 @@ contract Twitter {
         //to store tweets in array 
         //.push(_tweeet) added
         //uint i added in get tweet function
+
+        //adding like function
+        function likeTweet(address author, uint256 id) external 
+        {
+            require(tweets[author][id].id ==id,"Tweet Does not exist");
+            tweets[author][id].likes++;
+            
+        }
+
+        ///function to unlike
+        function unlikeTweet(address author,uint256 id) external 
+        {
+            require(tweets[author][id].id ==id,"Tweet Does not exist");
+            tweets[author][id].likes--;
+        }
 
     //to get the specific tweet
         function getTweet(uint _i) public view returns (Tweet memory){
@@ -57,4 +93,3 @@ contract Twitter {
 
 
 }
-
