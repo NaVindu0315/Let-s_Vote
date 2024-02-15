bb78fe999f5c5835bf858cece02ff1904cec1364
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-02-15 15:54:24 +0530

    added list other errors

---
+++ lib/pages/Attempts/List_other_errors.dart
@@ -0,0 +1,215 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../../firebase_options.dart';
+
+late User loggedinuser;
+late String client;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(other_errors_list());
+}
+
+class other_errors_list extends StatefulWidget {
+  @override
+  gemlistview createState() => gemlistview();
+}
+
+class gemlistview extends State<other_errors_list> {
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  final CollectionReference postsRef =
+      FirebaseFirestore.instance.collection('unknown Errors');
+
+  String searchValue = ''; // Track the search value
+
+  @override
+  void initState() {
+    super.initState();
+    getcurrentuser();
+  }
+
+  void getcurrentuser() async {
+    try {
+      // final user = await _auth.currentUser();
+      ///yata line eka chatgpt code ekk meka gatte uda line eke error ekk ena hinda hrytama scene eka terenne na
+      User? user = FirebaseAuth.instance.currentUser;
+      if (user != null) {
+        loggedinuser = user;
+        client = loggedinuser.email!;
+
+        ///i have to call the getdatafrm the function here and parse client as a parameter
+
+        print(loggedinuser.email);
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        resizeToAvoidBottomInset: false,
+        backgroundColor: Color(0xFFDBD6E5),
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back_ios_new_outlined,
+              color: Colors.black,
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+          title: Text(
+            'Other Failed Attempts',
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          backgroundColor: Color(0xFFDBD6E5),
+        ),
+        body: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: TextField(
+                onChanged: (value) {
+                  // Update the search value when the user types
+                  setState(() {
+                    searchValue = value;
+                  });
+                },
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(vertical: 15.0),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                    borderSide: BorderSide(width: 0.8),
+                  ),
+                  hintText: 'Search Here',
+                  prefixIcon: Icon(
+                    Icons.search,
+                    size: 30.0,
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: StreamBuilder<QuerySnapshot>(
+                stream: FirebaseFirestore.instance
+                    .collection('unknown Errors')
+                    .snapshots(),
+                //postsRef.snapshots(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return CircularProgressIndicator();
+                  }
+
+                  // Filter the data based on the search value
+                  final filteredData = snapshot.data!.docs.where((doc) {
+                    final id = doc.id;
+
+                    final search = searchValue.toLowerCase();
+                    return id.contains(search);
+                  }).toList();
+
+                  return ListView.builder(
+                    itemCount: filteredData.length,
+                    itemBuilder: (context, index) {
+                      final doc = filteredData[index];
+                      final data = doc.data() as Map<String, dynamic>;
+
+                      return Card(
+                        color: Color(0xFFA888EB),
+                        child: ListTile(
+                          leading: Icon(Icons.add),
+                          title: GestureDetector(
+                            onTap: () {
+                              /*  Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => NextPage(
+                                        gemcode: '${data['gem code']}')),
+                              );*/
+                              print('${data['gem code']}');
+                            },
+                            child: Ink(
+                              child: Text(
+                                doc.id,
+                                style: TextStyle(
+                                  color: Color(0xFF43468E),
+                                  fontSize: 18.0,
+                                ),
+                              ),
+                            ),
+                          ),
+                          trailing: Container(
+                            width: 40.0,
+                            height: 40.0,
+                            decoration: BoxDecoration(),
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                },
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(bottom: 20.0, top: 20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xFF43468E),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                      padding: EdgeInsets.symmetric(
+                          horizontal: 20.0, vertical: 12.0),
+                      textStyle:
+                          TextStyle(fontSize: 20.0, color: Color(0xFF43468E)),
+                    ),
+                    child: Text('Back'),
+                  ),
+                  SizedBox(width: 8.0),
+                  ElevatedButton(
+                    onPressed: () {
+                      /*Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => AddGemPage()),
+                      );*/
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xFF43468E),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                      padding: EdgeInsets.symmetric(
+                          horizontal: 20.0, vertical: 12.0),
+                      textStyle: TextStyle(fontSize: 20.0, color: Colors.white),
+                    ),
+                    child: Text('Add new'),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
