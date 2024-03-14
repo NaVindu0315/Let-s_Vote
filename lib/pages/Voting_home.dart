8cb2c68b8242563b4c9ad7c354a8bf57e0648980
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-03-14 10:20:02 +0530

    added function

--- lib/pages/Voting_home.dart
+++ lib/pages/Voting_home.dart
@@ -10,6 +10,16 @@ import 'package:lets_vote/pages/management_dashboard.dart';
 
 import '../Colors/colors.dart';
 
+import 'dart:convert';
+
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:http/http.dart'; //You can also import the browser version
+import 'package:web3dart/web3dart.dart';
+import 'package:quickalert/quickalert.dart';
+import 'dart:io';
+
 late User loggedinuser;
 late String client;
 
@@ -48,6 +58,209 @@ class _voting_homeState extends State<voting_home> {
       print(e);
     }
   }
+
+  ///for blockchain
+  ///
+  ///
+  var apiUrl =
+      "https://sepolia.infura.io/v3/4887f9655ec94842a2d3206deae69ad2"; //Replace with your API
+
+  var httpClient = Client();
+
+  var credentials = EthPrivateKey.fromHex(
+      "bdb3d39f69282abbce39e6d834b762a6ab093d97b94411f018b1fe607ea017e6");
+
+  ///to get
+  // Replace with placeholders (refer to security note above)
+  final String infuraEndpoint =
+      'https://sepolia.infura.io/v3/4887f9655ec94842a2d3206deae69ad2'; // Replace with actual endpoint
+  final String infuraApiKey =
+      '4887f9655ec94842a2d3206deae69ad2'; // Replace with actual API key
+  final String contractAddress = '0xc691a5f193883bE1Ef4d03f0c7f60De8B88913A3';
+
+  Future<void> getbalance() async {
+    var ethClient = Web3Client(apiUrl, httpClient);
+    var address = credentials.address;
+    EtherAmount balance = await ethClient.getBalance(address);
+    print(balance.getValueInUnit(EtherUnit.ether));
+  }
+
+  ///function begin
+
+  ///to get json from firebase
+
+  Future<String> getnamejson() async {
+    final ref = FirebaseStorage.instance.ref('c/namecontract.json');
+    final bytes = await ref.getData();
+    final jsonString = utf8.decode(bytes!);
+    return (jsonString);
+  }
+
+  Future<String> nameagejson() async {
+    final ref = FirebaseStorage.instance.ref('c/agecontract.json');
+    final bytes = await ref.getData();
+    final jsonString = utf8.decode(bytes!);
+    return (jsonString);
+  }
+
+  Future<String> getmyjson() async {
+    final ref = FirebaseStorage.instance.ref('c/contract.json');
+    final bytes = await ref.getData();
+    final jsonString = utf8.decode(bytes!);
+    return (jsonString);
+  }
+
+  ///end
+  /// function
+  /// new try
+
+  //late Web3Client ethClient;
+
+// Ethereum address
+  final String myAddress = "0x3C3f0990BAcd02C0ed689bf4Dd6CE18cD3D6A0bF";
+
+// URL from Infura
+  final String blockchainUrl =
+      "https://sepolia.infura.io/v3/4887f9655ec94842a2d3206deae69ad2";
+/*
+  Future<DeployedContract> getContract() async {
+    String contractAddress = "0xc691a5f193883bE1Ef4d03f0c7f60De8B88913A3";
+    // Obtain our smart contract using rootbundle to access our json file
+    /* String abiFile = await rootBundle.loadString("assets/contract.json");
+
+    final contract = DeployedContract(ContractAbi.fromJson(abiFile, "Voting"),
+        EthereumAddress.fromHex(contractAddress));*/
+    final abiFile = await getjson();
+    final contract = DeployedContract(ContractAbi.fromJson(abiFile, 'Voting'),
+        EthereumAddress.fromHex(contractAddress));
+    print("payyya");
+    return contract;
+
+    return contract;
+  }
+*/
+  ///functions to get contracts
+  Future<DeployedContract> getnamecontract() async {
+    String contractAddress = "0x3787D8F37054cf954c02eAF65C8b37FB97946de5";
+
+    final abiFile = await getnamejson();
+    final namecontract = DeployedContract(
+        ContractAbi.fromJson(abiFile, 'Voting'),
+        EthereumAddress.fromHex(contractAddress));
+    print("payyya");
+    return namecontract;
+  }
+
+  Future<DeployedContract> getagecontract() async {
+    String contractAddress = "0xd34780b7c47de1Cb09E81D4e9dE74a78CC821291";
+
+    final abiFile = await nameagejson();
+    final agecontract = DeployedContract(
+        ContractAbi.fromJson(abiFile, 'Voting'),
+        EthereumAddress.fromHex(contractAddress));
+    print("payyya");
+    return agecontract;
+  }
+
+  Future<DeployedContract> getfirst() async {
+    String contractAddress = "0xc691a5f193883bE1Ef4d03f0c7f60De8B88913A3";
+
+    final abiFile = await getmyjson();
+    final firstcontract = DeployedContract(
+        ContractAbi.fromJson(abiFile, 'Voting'),
+        EthereumAddress.fromHex(contractAddress));
+    print("payyya");
+    return firstcontract;
+  }
+
+  ///contract functions end
+
+  ///for name
+
+  late String newname;
+  Future<List<dynamic>> callnameFunction(String name) async {
+    final pakeclient = Web3Client(blockchainUrl, httpClient);
+    final contract = await getnamecontract();
+    final function = contract.function(name);
+    final result = await pakeclient
+        .call(contract: contract, function: function, params: []);
+    print(result);
+    if (result.length == 1 && result[0] is String) {
+      newname = result[0] as String;
+    } else {
+      newname = 'Result is not a string.';
+    }
+    QuickAlert.show(
+      context: context,
+      type: QuickAlertType.success,
+      text: '$newname',
+      autoCloseDuration: const Duration(seconds: 4),
+      showConfirmBtn: false,
+    );
+    return result;
+  }
+
+  late String values;
+  Future<List<dynamic>> callagefunction(String name) async {
+    final pakeclient = Web3Client(blockchainUrl, httpClient);
+    final contract = await getagecontract();
+    final function = contract.function(name);
+    final result = await pakeclient
+        .call(contract: contract, function: function, params: []);
+    print(result);
+    if (result.length == 1 && result[0] is String) {
+      values = result[0] as String;
+    } else {
+      values = 'Result is not a string.';
+    }
+    QuickAlert.show(
+      context: context,
+      type: QuickAlertType.success,
+      text: '$values',
+      autoCloseDuration: const Duration(seconds: 4),
+      showConfirmBtn: false,
+    );
+    return result;
+  }
+
+  late String didu;
+  Future<List<dynamic>> callfirstfunction(String name) async {
+    final pakeclient = Web3Client(blockchainUrl, httpClient);
+    final contract = await getfirst();
+    final function = contract.function(name);
+    final result = await pakeclient
+        .call(contract: contract, function: function, params: []);
+    print(result);
+    if (result.length == 1 && result[0] is String) {
+      didu = result[0] as String;
+    } else {
+      didu = 'Result is not a string.';
+    }
+    QuickAlert.show(
+      context: context,
+      type: QuickAlertType.success,
+      text: '$didu',
+      autoCloseDuration: const Duration(seconds: 4),
+      showConfirmBtn: false,
+    );
+    return result;
+  }
+
+  ///name end
+  ///
+  /// name and age contract
+
+  ///end
+
+  Future<void> loadJsonFromFirebase() async {
+    final ref = FirebaseStorage.instance.ref('c/contract.json');
+    final bytes = await ref.getData();
+    final jsonString = utf8.decode(bytes!);
+    print(jsonString);
+  }
+
+  ///
+  ///blockchain end
 
   @override
   Widget build(BuildContext context) {
