70641ba516420d66bf7c6bffcfaddd96e679c0da
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-04-17 10:23:21 +0530

    voting home 2

---
+++ lib/test_voting/vote_home_2.dart
@@ -0,0 +1,197 @@
+import 'package:flutter/material.dart';
+
+import 'package:http/http.dart';
+import 'package:lets_vote/test_voting/test_functions.dart';
+
+import 'package:web3dart/web3dart.dart';
+import 'test_constants.dart';
+import 'package:quickalert/quickalert.dart';
+
+class Vote_Home_2 extends StatefulWidget {
+  const Vote_Home_2({Key? key}) : super(key: key);
+
+  @override
+  State<Vote_Home_2> createState() => _Vote_Home_2State();
+}
+
+class _Vote_Home_2State extends State<Vote_Home_2> {
+  Client? httpClient;
+  Web3Client? ethClient;
+
+  @override
+  void initState() {
+    httpClient = Client();
+    ethClient = Web3Client(infura_url, httpClient!);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: [
+              Spacer(),
+
+              ///vote count display row
+              Row(
+                children: [
+                  Spacer(),
+                  Column(
+                    children: [
+                      FutureBuilder<List>(
+                          future: getvotes_1(ethClient!),
+                          builder: (context, snapshot) {
+                            if (snapshot.connectionState ==
+                                ConnectionState.waiting) {
+                              return Center(
+                                child: CircularProgressIndicator(),
+                              );
+                            }
+                            return Text(
+                              snapshot.data![0].toString(),
+                              style: TextStyle(
+                                  fontSize: 50, fontWeight: FontWeight.bold),
+                            );
+                          }),
+                    ],
+                  ),
+                  Spacer(),
+                  Column(
+                    children: [
+                      FutureBuilder<List>(
+                          future: getvotes_2(ethClient!),
+                          builder: (context, snapshot) {
+                            if (snapshot.connectionState ==
+                                ConnectionState.waiting) {
+                              return Center(
+                                child: CircularProgressIndicator(),
+                              );
+                            }
+                            return Text(
+                              snapshot.data![0].toString(),
+                              style: TextStyle(
+                                  fontSize: 50, fontWeight: FontWeight.bold),
+                            );
+                          }),
+                    ],
+                  ),
+                  Spacer(),
+                ],
+              ),
+
+              /// vote count display row end
+              SizedBox(
+                height: 10.0,
+              ),
+
+              ///row to display name of the candidates
+              Row(
+                children: [
+                  Spacer(),
+                  Column(
+                    children: [
+                      Text(
+                        'Candidate 1',
+                        style: TextStyle(fontSize: 30.0),
+                      ),
+                    ],
+                  ),
+                  Spacer(),
+                  Column(
+                    children: [
+                      Text(
+                        'Candidate 2',
+                        style: TextStyle(fontSize: 30.0),
+                      ),
+                    ],
+                  ),
+                  Spacer(),
+                ],
+              ),
+
+              ///candidate display row end
+
+              Spacer(),
+
+              ///row for the buttons
+              Row(
+                children: [
+                  Spacer(),
+                  Column(
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          vote_1(context, ethClient!);
+                        },
+                        child: Text(
+                          'Vote 1',
+                          style: TextStyle(fontSize: 30.0),
+                        ),
+                      )
+                    ],
+                  ),
+                  Spacer(),
+                  Column(
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          vote_2(context, ethClient!);
+                        },
+                        child: Text(
+                          'Vote 2',
+                          style: TextStyle(fontSize: 30.0),
+                        ),
+                      )
+                    ],
+                  ),
+                  Spacer(),
+                ],
+              ),
+              SizedBox(
+                height: 25.0,
+              ),
+
+              ///bbuttons row end
+              ///row for clear button
+              ///
+              Row(
+                children: [
+                  Spacer(),
+                  ElevatedButton(
+                      onPressed: () {
+                        clearall(context, ethClient!);
+                      },
+                      child: Text('Clear Vote Count')),
+                  Spacer(),
+                ],
+              ),
+
+              ///button row end
+              SizedBox(
+                height: 25.0,
+              ),
+
+              ///bbuttons row end
+              ///row for clear button
+              ///
+              Row(
+                children: [
+                  Spacer(),
+                  ElevatedButton(
+                      onPressed: () {
+                        setState(() {});
+                      },
+                      child: Text('Refresh')),
+                  Spacer(),
+                ],
+              ),
+              Spacer(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
