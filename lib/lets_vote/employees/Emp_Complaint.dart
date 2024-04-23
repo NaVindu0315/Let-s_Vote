56ce5d6cc25a3c28c18a07d70e5c7a0b7a180ae4
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-04-24 01:55:23 +0530

    page to send mail

--- lib/lets_vote/employees/Emp_Complaint.dart
+++ lib/lets_vote/employees/Emp_Complaint.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 import '../../Colors/colors.dart';
 import 'emp_dashboard.dart';
 
+import 'package:mailer/mailer.dart';
+import 'package:mailer/smtp_server.dart';
+
 class Emp_Complaints extends StatefulWidget {
   const Emp_Complaints({Key? key}) : super(key: key);
 
@@ -11,6 +14,33 @@ class Emp_Complaints extends StatefulWidget {
 }
 
 class _Emp_ComplaintsState extends State<Emp_Complaints> {
+  String username = 'letsvotelv2024@gmail.com';
+  String password = 'edpxfzzripyqjqms';
+
+  final smtpServer = gmail('letsvotelv2024@gmail.com', 'edpxfzzripyqjqms');
+  TextEditingController titlecontroller = TextEditingController();
+  TextEditingController msgcontrller = TextEditingController();
+  String titlee = "";
+  String msgg = "";
+
+  Future<void> MailSend(String title, String msg) async {
+    final message = Message()
+      ..from = Address(username)
+      ..recipients.add('electionofficerletsvote@gmail.com')
+      ..subject = title
+      ..text = msg;
+
+    try {
+      final sendReport = await send(message, smtpServer);
+      print('Message sent: ' + sendReport.toString());
+    } on MailerException catch (e) {
+      print('Message not sent.');
+      for (var p in e.problems) {
+        print('Problem: ${p.code}: ${p.msg}');
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
