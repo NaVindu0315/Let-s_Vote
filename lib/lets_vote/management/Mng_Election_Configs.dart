b83e7733afab784d80ec41783497b5a90e1d2321
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-04-25 02:08:40 +0530

    config working

--- lib/lets_vote/management/Mng_Election_Configs.dart
+++ lib/lets_vote/management/Mng_Election_Configs.dart
@@ -19,7 +19,7 @@ class _Mng_Election_configsState extends State<Mng_Election_configs> {
   int iselection = 0;
   int isresults = 0;
 
-  double setlvl = 0.0;
+  late double setlvl;
 
   TextEditingController lvlcontroller = TextEditingController();
 
@@ -111,163 +111,184 @@ class _Mng_Election_configsState extends State<Mng_Election_configs> {
 
           //centerTitle: true,
         ),
-        body: Column(
-          children: [
-            SizedBox(
-              height: 10.0,
-            ),
-            Row(
-              children: [
-                Spacer(),
-                Text(
-                  'Change Configs',
-                  style:
-                      TextStyle(fontSize: 30.0, color: AppColors.buttoncolor),
-                ),
-                Spacer(),
-              ],
-            ),
-            SizedBox(
-              height: 50.0,
-            ),
-            Row(
-              children: [
-                Spacer(),
-                Text(
-                  'Current Similarity Level - $lvl',
-                  style: TextStyle(fontSize: 25.0, color: Colors.white),
-                ),
-                Spacer(),
-              ],
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            Row(
-              children: [
-                SizedBox(
-                  height: 70,
-                  width: 300, // Set the width of the SizedBox to 300 pixels
-                  child: Card(
-                    elevation: 10,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: TextFormField(
-                      controller: lvlcontroller,
-                      onChanged: (value) {
-                        //email = value;
-                        setlvl = value as double;
-                      },
-                      decoration: InputDecoration(
-                        prefixIcon: Icon(
-                          Icons.summarize_outlined,
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 10.0,
+              ),
+              Row(
+                children: [
+                  Spacer(),
+                  Text(
+                    'Change Configs',
+                    style:
+                        TextStyle(fontSize: 30.0, color: AppColors.buttoncolor),
+                  ),
+                  Spacer(),
+                ],
+              ),
+              SizedBox(
+                height: 50.0,
+              ),
+              Row(
+                children: [
+                  Spacer(),
+                  Text(
+                    'Current Similarity Level - $lvl',
+                    style: TextStyle(fontSize: 25.0, color: Colors.white),
+                  ),
+                  Spacer(),
+                ],
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Row(
+                children: [
+                  SizedBox(
+                    height: 70,
+                    width: 300, // Set the width of the SizedBox to 300 pixels
+                    child: Card(
+                      elevation: 10,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: TextFormField(
+                        controller: lvlcontroller,
+                        onChanged: (value) {
+                          //email = value;
+                          setlvl = double.parse(value);
+                        },
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(
+                            Icons.summarize_outlined,
+                          ),
+                          labelText: 'New Similarity level',
+                          border: OutlineInputBorder(),
                         ),
-                        labelText: 'New Similarity level',
-                        border: OutlineInputBorder(),
                       ),
                     ),
                   ),
-                ),
-                ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: AppColors.buttoncolor,
-                    ),
-                    onPressed: () {},
-                    child: Text(
-                      'Set',
-                      style: TextStyle(
-                          fontSize: 30.0, color: AppColors.backgroundcolor),
-                    )),
-              ],
-            ),
-            SizedBox(
-              height: 50.0,
-            ),
-            Row(
-              children: [
-                Spacer(),
-                Text(
-                  'Voting Enabled  $iselection',
-                  style: TextStyle(fontSize: 25.0, color: Colors.white),
-                ),
-                Spacer(),
-              ],
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            Row(
-              children: [
-                Spacer(),
-                ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: AppColors.buttoncolor,
-                    ),
-                    onPressed: () {},
-                    child: Text(
-                      'Enable',
-                      style: TextStyle(
-                          fontSize: 30.0, color: AppColors.backgroundcolor),
-                    )),
-                Spacer(),
-                ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: AppColors.buttoncolor,
-                    ),
-                    onPressed: () {},
-                    child: Text(
-                      'Disable',
-                      style: TextStyle(
-                          fontSize: 30.0, color: AppColors.backgroundcolor),
-                    )),
-                Spacer(),
-              ],
-            ),
-            SizedBox(
-              height: 50.0,
-            ),
-            Row(
-              children: [
-                Spacer(),
-                Text(
-                  'Results Enabled  $isresults',
-                  style: TextStyle(fontSize: 25.0, color: Colors.white),
-                ),
-                Spacer(),
-              ],
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            Row(
-              children: [
-                Spacer(),
-                ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: AppColors.buttoncolor,
-                    ),
-                    onPressed: () {},
-                    child: Text(
-                      'Enable',
-                      style: TextStyle(
-                          fontSize: 30.0, color: AppColors.backgroundcolor),
-                    )),
-                Spacer(),
-                ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: AppColors.buttoncolor,
-                    ),
-                    onPressed: () {},
-                    child: Text(
-                      'Disable',
-                      style: TextStyle(
-                          fontSize: 30.0, color: AppColors.backgroundcolor),
-                    )),
-                Spacer(),
-              ],
-            ),
-          ],
+                  ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: AppColors.buttoncolor,
+                      ),
+                      onPressed: () {
+                        levelset(setlvl);
+                        lvlcontroller.clear();
+                      },
+                      child: Text(
+                        'Set',
+                        style: TextStyle(
+                            fontSize: 30.0, color: AppColors.backgroundcolor),
+                      )),
+                ],
+              ),
+              SizedBox(
+                height: 50.0,
+              ),
+              Row(
+                children: [
+                  Spacer(),
+                  Text(
+                    iselection == 1 ? 'Voting Enabled ' : 'Voting Disabled',
+                    style: TextStyle(fontSize: 25.0, color: Colors.white),
+                  ),
+                  Spacer(),
+                ],
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Row(
+                children: [
+                  Spacer(),
+                  ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: AppColors.buttoncolor,
+                      ),
+                      onPressed: iselection == 0
+                          ? () {
+                              enableelection();
+                            }
+                          : null,
+                      child: Text(
+                        'Enable',
+                        style: TextStyle(
+                            fontSize: 30.0, color: AppColors.backgroundcolor),
+                      )),
+                  Spacer(),
+                  ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: AppColors.buttoncolor,
+                      ),
+                      onPressed: iselection == 1
+                          ? () {
+                              disableelection();
+                            }
+                          : null,
+                      child: Text(
+                        'Disable',
+                        style: TextStyle(
+                            fontSize: 30.0, color: AppColors.backgroundcolor),
+                      )),
+                  Spacer(),
+                ],
+              ),
+              SizedBox(
+                height: 50.0,
+              ),
+              Row(
+                children: [
+                  Spacer(),
+                  Text(
+                    isresults == 1 ? 'Results Enabled ' : 'Results Disabled',
+                    style: TextStyle(fontSize: 25.0, color: Colors.white),
+                  ),
+                  Spacer(),
+                ],
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Row(
+                children: [
+                  Spacer(),
+                  ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: AppColors.buttoncolor,
+                      ),
+                      onPressed: isresults == 0
+                          ? () {
+                              enableresults();
+                            }
+                          : null,
+                      child: Text(
+                        'Enable',
+                        style: TextStyle(
+                            fontSize: 30.0, color: AppColors.backgroundcolor),
+                      )),
+                  Spacer(),
+                  ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: AppColors.buttoncolor,
+                      ),
+                      onPressed: isresults == 1
+                          ? () {
+                              disableresults();
+                            }
+                          : null,
+                      child: Text(
+                        'Disable',
+                        style: TextStyle(
+                            fontSize: 30.0, color: AppColors.backgroundcolor),
+                      )),
+                  Spacer(),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
