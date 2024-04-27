c7b92f44f5e44f5c01c94c7695dadc0c45634131
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-04-27 21:32:41 +0530

    test

--- lib/test_voting/test_constants.dart
+++ lib/test_voting/test_constants.dart
@@ -8,3 +8,5 @@ String voter_private_key =
     "bdb3d39f69282abbce39e6d834b762a6ab093d97b94411f018b1fe607ea017e6";
 
 String contractAddress1 = "0x250270D8E311658531f2aa70f5193Ee3F406129C";
+
+String newkey = "";
