a0b68003b8a2d30b5896731018a8a29c2b11edfd
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-04-27 21:44:10 +0530

    removed constants

---
+++ lib/test_voting/test_constants.dart
@@ -0,0 +1,12 @@
+String infura_url =
+    "https://sepolia.infura.io/v3/4887f9655ec94842a2d3206deae69ad2";
+
+String owner_private_key =
+    "bdb3d39f69282abbce39e6d834b762a6ab093d97b94411f018b1fe607ea017e6";
+
+String voter_private_key =
+    "bdb3d39f69282abbce39e6d834b762a6ab093d97b94411f018b1fe607ea017e6";
+
+String contractAddress1 = "0x250270D8E311658531f2aa70f5193Ee3F406129C";
+
+String newkey = "";
