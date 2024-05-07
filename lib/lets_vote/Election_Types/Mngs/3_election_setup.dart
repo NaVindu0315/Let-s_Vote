f511a9912837493f2b02a6d8bd1a69623d6de25a
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-05-08 00:50:39 +0530

    new pages for election setup

---
+++ lib/lets_vote/Election_Types/Mngs/3_election_setup.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import '../../../Colors/colors.dart';
+import '../../management/mng_election_select_page.dart';
+
+class election_setup_3 extends StatefulWidget {
+  const election_setup_3({Key? key}) : super(key: key);
+
+  @override
+  State<election_setup_3> createState() => _election_setup_3State();
+}
+
+class _election_setup_3State extends State<election_setup_3> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: AppColors.backgroundcolor,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          // preferredSize: Size.fromHeight(kToolbarHeight + 20),
+          backgroundColor: AppColors.backgroundcolor,
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => mng_election_type_select()),
+              ); // go back to the previous screen
+            },
+          ),
+
+          title: Text(
+            '2 Person Election ',
+            style: TextStyle(fontSize: 26.0, color: AppColors.buttoncolor),
+          ),
+          iconTheme: IconThemeData(color: Colors.white),
+
+          //centerTitle: true,
+        ),
+      ),
+    );
+  }
+}
